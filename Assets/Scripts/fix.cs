dab894aecd0e6b5d21c4482748beee3afc577b2e
nic1207 <nic1207@gmail.com>
2017-02-08 10:33:37 +0800

    UGUI 應對各種螢幕自動調整大小及位置

---
+++ Assets/Scripts/fix.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class fix : MonoBehaviour {
+
+	void Awake(){
+
+		CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
+
+		float screenWidthScale = Screen.width / canvasScaler.referenceResolution.x;
+		float screenHeightScale = Screen.height / canvasScaler.referenceResolution.y;
+
+		canvasScaler.matchWidthOrHeight = screenWidthScale > screenHeightScale ? 1 : 0;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
